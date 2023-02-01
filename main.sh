62995914b20acdde99f4fe4a6cad631836f8f0a2
Kepler Despinos <kepler.despinos@fox.com>
2023-02-01 11:45:51 -0800

    Version 2.0 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- main.sh
+++ main.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-
-
-echo enter os
-read os
-
 main () {
-    if [[ $os = "win" ]]
-    then 
-        echo enter instance id
+    echo which os? [windows/linux]
+    read os
+    if [[ $os = "windows" || $os = "Windows" || $os = "win" ]]
+    then
+        var=0
+        echo enter instance id 
         read instance_id
         echo enter cli profile
         read cli_profile
         aws ssm start-session --target $instance_id --region us-west-2 --document-name AWS-StartPortForwardingSession --parameters=localPortNumber=55678,portNumber=3389 --profile $cli_profile
+    elif [[ $os = "linux" || $os = "Linux" ]]
+    then
+        var=0
+        echo enter instance id
+        read instance_id
+        echo enter cli profile
+        read cli_profile
+        aws ssm start-session --target $instance_id --region us-west-2 --profile $cli_profile
     else
-        windows
+        main
     fi
 }
 
 main
+
