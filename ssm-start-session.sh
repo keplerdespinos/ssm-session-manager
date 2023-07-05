02c1858e996cfca587ab23c3fcfa3fc2272efc3e
Kepler Despinos <kepler.despinos@fox.com>
2023-07-05 09:00:57 -0700

    adding multi-region support

--- ssm-start-session.sh
+++ ssm-start-session.sh
@@ -7,18 +7,22 @@ main () {
         var=0
         echo enter instance id 
         read instance_id
+        echo enter region
+        read aws_region
         echo enter cli profile
         read cli_profile
         echo open microsoft remote desktop and connect to "localhost:55678"
-        aws ssm start-session --target $instance_id --region us-west-2 --document-name AWS-StartPortForwardingSession --parameters=localPortNumber=55678,portNumber=3389 --profile $cli_profile
+        aws ssm start-session --target $instance_id --region $aws_region --document-name AWS-StartPortForwardingSession --parameters=localPortNumber=55678,portNumber=3389 --profile $cli_profile
     elif [[ $os = "linux" || $os = "Linux" ]]
     then
         var=0
         echo enter instance id
         read instance_id
+        echo enter region
+        read aws_region
         echo enter cli profile
         read cli_profile
-        aws ssm start-session --target $instance_id --region us-west-2 --profile $cli_profile
+        aws ssm start-session --target $instance_id --region $aws_region --profile $cli_profile
     else
         main
     fi
